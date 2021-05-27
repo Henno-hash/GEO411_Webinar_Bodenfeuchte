31a4a163e295e075465de9fa4ad1c67ebba50235
Henno-hash <HenrikS94@gmx.de>
2021-05-27 15:43:18 +0200

    removed whitespaces

--- Scripts/GEO411radar.py
+++ Scripts/GEO411radar.py
@@ -1,7 +1,5 @@
-import glob
 import pandas as pd
 import io
-
 file='E:/UNI/GEO411_Bodenfeuchte/S1Rueckstreuung/20mPixel/Rueckstreuwerte/point_per_pixel_VH_Asc.csv'
 f = open(file, "r")
 temp=f.read()
@@ -9,7 +7,6 @@ temp=f.read()
 temp = temp.replace("# ", "")
 # search and replace double tabs "\t\t" in the string "temp"
 temp = temp.replace("\t\t", "")
-
 # import the string into a panda data frame
 vh_asc = pd.read_csv(io.StringIO(temp), sep=',', dtype=str)
 vh_asc['date'] = pd.to_datetime(vh_asc['date'], format='%Y%m%d')
